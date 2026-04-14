935153f0b768ed5cb4b78f71adfda870cb415c5a
Shubham <shubh@Shubhams-MacBook-Air.local>
2026-04-14 18:53:17 +0530

    feat: Add Meta Pixel tracking to Travel Assistance landing pages

--- src/app/travel-assistance-savings/page.tsx
+++ src/app/travel-assistance-savings/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import MitrNavbar from "@/components/travel-mitr/MitrNavbar";
 import TravelHero from "@/components/travel-mitr/TravelHero";
 import HelpCards from "@/components/travel-mitr/HelpCards";
@@ -18,6 +19,12 @@ export const metadata: Metadata = {
 export default function TravelMitrSavingsPage() {
   return (
     <div className="min-h-screen bg-white">
+      <Script id="meta-pixel-tas" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1972374793664843');fbq('track','PageView');`}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1972374793664843&ev=PageView&noscript=1" alt="" />
+      </noscript>
       <MitrNavbar />
       <TravelHero
         headline={TRAVEL_SAVINGS_HERO.headline}
